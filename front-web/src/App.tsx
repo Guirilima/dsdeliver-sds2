31aa185d9e22b10c706d62fe2240023d5ff5946f
Guirilima <ungui@outlook.com.br>
2021-01-07 21:36:24 -0300

    Created front web initial files

---
+++ front-web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Hello from './Hello';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
