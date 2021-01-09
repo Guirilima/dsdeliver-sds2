eb2d27004b255fa15c1edceab21e84926a90484b
Guirilima <ungui@outlook.com.br>
2021-01-09 15:01:45 -0300

    Add StepsHeader

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,11 @@
+import StepsHeader from './StepsHeader';
 import './styles.css';
 
 
 function Orders() {
     return (
-        <div>
-            <h1>ORders</h1>
+        <div className="orders-container">
+            <StepsHeader />
         </div>
     )
 }
